abcf2ce37171f7fcf8ddc08ed3ff2accf5cad3b4
萌叔 <1580847566@qq.com>
2019-05-26 21:17:23 +0800

    test

---
+++ src/com/neuedu/test/lianxiti.java
@@ -0,0 +1,15 @@
+package com.neuedu.test;
+
+public class lianxiti {
+    public static void main(String[] args) {
+       int i=153;
+       int a=0;
+        while (i!= 0) {
+            a=i%10;
+            System.out.println(a);
+            i=i/10;
+
+        }
+
+    }
+    }
