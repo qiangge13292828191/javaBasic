aa3ebcb3175c5180cca1242497d41497c787ae5d
萌叔 <1580847566@qq.com>
2019-05-29 20:12:03 +0800

    test

---
+++ src/com/neuedu/second/Instrument.java
@@ -0,0 +1,7 @@
+package com.neuedu.second;
+
+public class Instrument {
+    public void play(){
+        System.out.println();
+    }
+}
---
+++ src/com/neuedu/second/Wind.java
@@ -0,0 +1,7 @@
+package com.neuedu.second;
+
+public class Wind extends Instrument{
+    public void play(){
+        System.out.println("wind");
+    }
+}
